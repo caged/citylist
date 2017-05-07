ed9325f84b963c3f4c1f5346af28e1bf818fcf0c
Caged <justin@labratrevenge.com>
2017-05-07 13:42:42 -0700

    add static image tiles

--- app/models/channel.py
+++ app/models/channel.py
@@ -1,10 +1,16 @@
 from datetime import datetime
+from urllib.request import pathname2url
 import re
+import shutil
+import textwrap
+import os
 import sqlalchemy as sa
 from sqlalchemy import event, orm
 from app.db import Base
 import geocoder
-import textwrap
+import requests
+
+public_token = 'pk.eyJ1IjoiY2FnZWQiLCJhIjoiQjd2aXNGYyJ9.gr1QeGYwG1QYUW47I-DqaQ'
 
 
 class Channel(Base):
@@ -28,6 +34,7 @@ class Channel(Base):
     def init_on_load(self):
         self.proposal = self.extract_proposal()
         self.excerpt = self.extract_proposal_excerpt()
+
         try:
             self.title, self.notice = self.description.split(' - ')
             self.notice_class = self.notice.lower().replace(' ', '-')
@@ -43,6 +50,48 @@ class Channel(Base):
 
     def extract_proposal_excerpt(self):
         return textwrap.shorten(self.proposal, width=350)
+
+    def cached_map_image(self, width=300, height=300, zoom=14, pitch=0, bearing=0,
+                         default='/static/tiles/na.png'):
+
+        name = '%s-%s-%s-%s-%s-%s.png' % (self.id, width, height, zoom, pitch, bearing)
+        file_name = '/static/tiles/%s' % name
+        if os.path.isfile('app' + file_name):
+            return file_name
+        else:
+            file_name = default
+
+        try:
+            geojson = self.geojson_point()
+            url = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/geojson(%s)/%s,%s,%s,%s,%s/%sx%s?access_token=%s" % (
+                pathname2url(geojson),
+                self.lon,
+                self.lat,
+                zoom,
+                bearing,
+                pitch,
+                width,
+                height,
+                public_token
+            )
+
+            print(url)
+            img = requests.get(url)
+
+            if img.status_code == 200:
+                name = '%s-%s-%s-%s-%s-%s.png' % (self.id, width, height, zoom, pitch, bearing)
+                file_name = '/static/tiles/%s' % name
+                with open('app' + file_name, 'wb') as f:
+                    f.write(img.content)
+
+            return file_name
+
+        except Exception as e:
+            raise
+
+
+    def geojson_point(self):
+        return '{"type": "Point","coordinates": [%s, %s]}' % (self.lon, self.lat)
 
     @classmethod
     def generate_slug(cls, date, case):
