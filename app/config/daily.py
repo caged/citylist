a6c550d446fc4a37209bfd3697f2588c9cb2c38d
Caged <justin@labratrevenge.com>
2017-05-07 18:06:06 -0700

    temporarily disable full import while I work out some production import issues

--- app/config/daily.py
+++ app/config/daily.py
@@ -4,30 +4,30 @@ BDSPublicNoticeImporter(
     name='Central Northeast Neighbors',
     url='https://www.portlandoregon.gov/rss.cfm?c=42257')
 
-BDSPublicNoticeImporter(
-    name='East Portland Neighborhood Office',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42258')
-
-BDSPublicNoticeImporter(
-    name='Neighbors West/Northwest',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42259')
-
-BDSPublicNoticeImporter(
-    name='North Portland Neighborhood Services',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42261')
-
-BDSPublicNoticeImporter(
-    name='Northeast Coalition of Neighbors',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42260')
-
-BDSPublicNoticeImporter(
-    name='Southeast Uplift Neighborhood Program',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42262')
-
-BDSPublicNoticeImporter(
-    name='Southwest Neighborhoods, Inc.',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42263')
-
-BDSPublicNoticeImporter(
-    name='Unincorporated & Unclaimed Area',
-    url='https://www.portlandoregon.gov/rss.cfm?c=42264')
+# BDSPublicNoticeImporter(
+#     name='East Portland Neighborhood Office',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42258')
+#
+# BDSPublicNoticeImporter(
+#     name='Neighbors West/Northwest',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42259')
+#
+# BDSPublicNoticeImporter(
+#     name='North Portland Neighborhood Services',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42261')
+#
+# BDSPublicNoticeImporter(
+#     name='Northeast Coalition of Neighbors',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42260')
+#
+# BDSPublicNoticeImporter(
+#     name='Southeast Uplift Neighborhood Program',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42262')
+#
+# BDSPublicNoticeImporter(
+#     name='Southwest Neighborhoods, Inc.',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42263')
+#
+# BDSPublicNoticeImporter(
+#     name='Unincorporated & Unclaimed Area',
+#     url='https://www.portlandoregon.gov/rss.cfm?c=42264')
